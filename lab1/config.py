7a8da704ebdcf1d78ac2fa6b2c8021b811a19030
t-linguist <92468855+t-linguist@users.noreply.github.com>
2023-05-15 15:15:59 +0300

    fix: change constants in config.py

--- lab1/config.py
+++ lab1/config.py
@@ -1,18 +1,19 @@
+import os
 import logging
 
 LOG_LEVEL = logging.INFO
 
-CURRENT_DIR = "./"
+BASE_DIR = "./"
 
-TRAIN_DIR = "./train"
-TEST_DIR = "./test"
-MODEL_DIR = "/.models"
+TRAIN_DIR = os.path.join(BASE_DIR, "train")
+TEST_DIR = os.path.join(BASE_DIR, "test")
+MODEL_DIR = os.path.join(BASE_DIR, "models")
 
-X_TRAIN_PATH = "./train/x_train.npy"
-X_TEST_PATH = "./test/x_test.npy"
-X_TRAIN_SCALED_PATH = "./train/x_train_scaled.npy"
-X_TEST_SCALED_PATH = "./test/x_test_scaled.npy"
-Y_TRAIN_PATH = "./train/y_train.npy"
-Y_TEST_PATH = "./test/y_test.npy"
+X_TRAIN_PATH = os.path.join(TRAIN_DIR, "x_train.npy")
+X_TEST_PATH = os.path.join(TEST_DIR, "x_test.npy")
+X_TRAIN_SCALED_PATH = os.path.join(TRAIN_DIR, "x_train_scaled.npy")
+X_TEST_SCALED_PATH = os.path.join(TEST_DIR, "x_test_scaled.npy")
+Y_TRAIN_PATH = os.path.join(TRAIN_DIR, "y_train.npy")
+Y_TEST_PATH = os.path.join(TEST_DIR, "y_test.npy")
 
-MODEL_PATH = "./models/lin_reg.pkl"
+MODEL_PATH = os.path.join(MODEL_DIR, "lin_reg.pkl")
