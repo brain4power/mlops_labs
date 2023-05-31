5a424c4c69e98570e0de9e42da493bf04f2dcb32
t-linguist <92468855+t-linguist@users.noreply.github.com>
2023-05-31 16:45:34 +0300

    feat: added column_transformer

--- lab2/data_preprocessing.py
+++ lab2/data_preprocessing.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 
 import pandas as pd
+from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from config import (LOG_LEVEL, X_TEST_PATH, X_TEST_SCALED_PATH, X_TRAIN_PATH,
@@ -12,7 +13,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(LOG_LEVEL)
 
 
-def split_features(df) -> dict:
+def split_features(df) -> tuple:
     column_names = df.columns.to_list()
     cat_columns = []
     num_columns = []
@@ -22,35 +23,25 @@ def split_features(df) -> dict:
             num_columns += [column_name]
         else:
             cat_columns += [column_name]
-    return {"num_columns": num_columns, "cat_columns": cat_columns}
+    return num_columns, cat_columns
 
 
 def preprocess_data() -> None:
     x_train = pd.read_csv(X_TRAIN_PATH)
     x_test = pd.read_csv(X_TEST_PATH)
 
-    train_num_columns = split_features(x_train)["num_columns"]
-    train_cat_columns = split_features(x_train)["cat_columns"]
-    test_num_columns = split_features(x_test)["num_columns"]
-    test_cat_columns = split_features(x_test)["cat_columns"]
+    num_cols, cat_cols = split_features(x_train)
 
-    sc = StandardScaler()
-    x_train_sc = sc.fit_transform(x_train[train_num_columns])
-    x_test_sc = sc.transform(x_test[test_num_columns])
-
-    ohe = OneHotEncoder(drop="if_binary", handle_unknown="ignore", sparse_output=False)
-    x_train_ohe = ohe.fit_transform(x_train[train_cat_columns])
-    x_test_ohe = ohe.transform(x_test[test_cat_columns])
-
-    x_train_scaled = pd.concat(
-        [pd.DataFrame(x_train_sc), pd.DataFrame(x_train_ohe)], axis=1
+    preprocessors = make_column_transformer(
+        (StandardScaler(), num_cols),
+        (OneHotEncoder(drop="if_binary", handle_unknown="ignore"), cat_cols)
     )
-    x_test_scaled = pd.concat(
-        [pd.DataFrame(x_test_sc), pd.DataFrame(x_test_ohe)], axis=1
-    )
-
-    x_train_scaled.to_csv(X_TRAIN_SCALED_PATH, index=False)
-    x_test_scaled.to_csv(X_TEST_SCALED_PATH, index=False)
+   
+    x_train_scaled = preprocessors.fit_transform(x_train)
+    x_test_scaled = preprocessors.transform(x_test)
+    
+    pd.DataFrame(x_train_scaled).to_csv(X_TRAIN_SCALED_PATH, index=False)
+    pd.DataFrame(x_test_scaled).to_csv(X_TEST_SCALED_PATH, index=False
 
 
 if __name__ == "__main__":
